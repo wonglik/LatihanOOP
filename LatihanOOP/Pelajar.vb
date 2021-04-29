3cb2576ac83c4f125013a107db47dc3f0faf8ffc
wong lik <wonglik0817@outlook.com>
2021-04-29 11:53:24 +0800

    Add project files.

---
+++ LatihanOOP/Pelajar.vb
@@ -0,0 +1,41 @@
+﻿Public Class Pelajar
+
+    Private strNoMatrik As String
+
+    Sub New(ByVal strNoMatrik As String)
+        Me.strNoMatrik = strNoMatrik
+    End Sub
+
+    Public Function Politeknik() As String
+        Dim strKodPoli As String = strNoMatrik.Substring(0, 2)
+
+        If strKodPoli = 18 Then
+            Return "PTSS"
+        Else
+            Return "Not PTSS"
+        End If
+    End Function
+
+    Public Function Program() As String
+        Dim strKodPogram As String = strNoMatrik.Substring(2, 3)
+
+        If strKodPogram = "DDT" Then
+            Return "Diploma in Digital Technology"
+        Else
+            Return "Not Diploma in Digital Technology"
+        End If
+    End Function
+
+    Public Function Ambilan() As String
+        Dim strTahun As String = strNoMatrik.Substring(5, 2)
+        Dim strSesi As String = strNoMatrik.Substring(8, 1)
+
+        If strTahun = "19" And strSesi = "1" Then
+            Return "Jun 2019"
+        ElseIf strTahun = "19" And strSesi = "2" Then
+            Return "Dec 2019"
+        Else
+            Return "Not 2019"
+        End If
+    End Function
+End Class
